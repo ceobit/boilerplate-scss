d3598eb11bb1315f450991bd693d12e53b5cab61
Roman Lebedenko <ceobit90@gmail.com>
2021-04-14 12:34:43 +0200

    added Webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './assets/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test:/\.(s*)css$/,
+      use: [
+        miniCss.loader,
+        'css-loader',
+        'sass-loader',
+      ]
+    }]
+  },
+  plugins: [
+    new miniCss({
+      filename: 'style.css',
+    }),
+    new HTMLWebpackPlugin({
+      inject: true,
+      template: './index.html',
+      filename: './index.html',
+      minify: {
+        collapseWhitespace: true //Минимифицируем index.html
+      }
+    }),
+  ]
+};
