b49f16cb235bafa6456c2fba08a313a4521fe5ca
Roman Lebedenko <ceobit90@gmail.com>
2021-04-20 09:13:42 +0200

    fixed an image path issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'assets'),
@@ -11,20 +12,47 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
     historyApiFallback: true,
     port: 3000,
     hot: true,
     open: true,
   },
   module: {
-    rules: [{
-      test:/\.(s*)css$/,
-      use: [
-        miniCss.loader,
-        'css-loader',
-        'sass-loader',
-      ]
-    }]
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: miniCss.loader,
+          },
+          'css-loader',
+          'resolve-url-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(ttf|woff2|woff)$/,
+        use: {
+          loader: 'file-loader?name=./fonts/[name].[ext]',
+          options: {
+            esModule: false,
+          },
+        },
+      },
+      {// compress images
+        test: /\.(png|jpg|gif|ico|svg)$/i,
+        use: [
+          'file-loader?name=./images/[name].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              esModule: false
+            },
+          },
+        ]
+      },
+    ],
   },
   plugins: [
     new miniCss({
@@ -33,11 +61,20 @@ module.exports = {
     new HTMLWebpackPlugin({
       inject: true,
       template: 'index.html',
-      filename: '[name].[hash].html',
+      filename: 'index.html',
       minify: {
-        collapseWhitespace: true
-      }
+        collapseWhitespace: true,
+      },
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'assets/images/img'),
+          to: path.resolve(__dirname, 'dist/images'),
+          force: true,
+        },
+      ],
+    }),
   ],
 };
